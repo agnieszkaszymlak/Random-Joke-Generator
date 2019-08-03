92813adbf65d4677ffe03213165c9be7fa395ff5
Agnieszka Szymlak <agniepiech@gmail.com>
2019-08-03 18:14:45 +0200

    Modify js source in index.html

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 'use strict';
 
-var url = 'http://api.icndb.com/jokes/random';
+// Create variable "url" containing the address to our joke
+var url = 'https://api.icndb.com/jokes/random';
 
+// Listening to the button with a random joke
 var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-  getJoke();
+button.addEventListener('click', function () {
+    getJoke();
 });
 
+// For the variable paragraph, we will assign the DOM element of the paragraph, which is responsible for displaying the wit
 var paragraph = document.getElementById('joke');
 
+// Implementation function getJoke
 function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function () {
+        var response = JSON.parse(xhr.response);
+        paragraph.innerHTML = response.value.joke;
+    });
+    xhr.send();
 }
-
-var response = JSON.parse(xhr.response);
 
 getJoke();
